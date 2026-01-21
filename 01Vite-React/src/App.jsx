0e08c95043aaac49228123939d2ad8f60028d9f1
syed-saad-1 <sfurqanarif7528@gmail.com>
2026-01-20 17:33:41 -1200

    React-Learning

---
+++ 01Vite-React/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>React With Vite || Syed Saad Raza</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Assalam-u-Alikum || Syed Saad Raza</h1>
+      <p>I am Saad and I Want to become a Engineer</p>
+    </div>
+  );
+}
+
+export default App;
